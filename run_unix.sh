d756a01ecca675487ea4caed0a4a04d711273f9e
Alexandre Serra <almateser@gmail.com>
2020-11-12 20:38:31 +0000

    Update Procfile

--- run_unix.sh
+++ run_unix.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-pip3 install -r requirements.txt;
 source venv/bin/activate;
+pip3 install -r requirements.txt;
 export FLASK_APP=app:create_app;
 export FLASK_ENV=development;
 export SECRET_KEY=`python -c 'import os; print(os.urandom(32))'`;
