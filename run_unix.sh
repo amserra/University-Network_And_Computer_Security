01ed35ef64a790760ec64af094cd1c3990b34344
Alexandre Serra <almateser@gmail.com>
2020-11-12 19:48:34 +0000

    Update for production

--- run_unix.sh
+++ run_unix.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 pip3 install -r requirements.txt;
-export FLASK_APP=app;
+source venv/bin/activate;
+export FLASK_APP=app:create_app;
 export FLASK_ENV=development;
 export SECRET_KEY=`python -c 'import os; print(os.urandom(32))'`;
 flask init-db;
