527fe182a9e5330ba0a3a30115764ac7d538d743
Alexandre Serra <almateser@gmail.com>
2020-11-21 22:55:07 +0000

    Update schema.sql

--- app/model/schema.sql
+++ app/model/schema.sql
@@ -1,12 +1,12 @@
 -- Initialize the database.
--- Drop any existing data and create empty tables.
+-- Drop any existing data and create empty tables (comment if you wish to keep data).
 
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
   email TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+  password TEXT NOT NULL,
+  emailVerified INTEGER NOT NULL DEFAULT 0
 );
